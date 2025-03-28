f45d4be4d1c08a5e83f7dd39d5a5830595140da5
las_r <128936184+las-r@users.noreply.github.com>
2025-03-28 16:33:41 -0400

    Dvd logo color change upon bounce

--- dvd-logo.py
+++ dvd-logo.py
@@ -5,11 +5,18 @@ import time
 # initialize
 pygame.init()
 
+# dvd color change
+def dvdColor():
+    global dvd
+    
+    oDvd = dvd
+    while dvd == oDvd:
+        dvd = random.choice([dvdb, dvdc, dvdg, dvdm, dvdr, dvdw, dvdy])
+
 # settings
 WIDTH, HEIGHT = 800, 600
-DVDIMG = "images/dvd.png"
 DVDWIDTH, DVDHEIGHT = 100, 44
-DVDSPEED = 500
+DVDSPEED = 400
 BGCOLOR = (0, 0, 0)
 
 # variables
@@ -18,8 +25,15 @@ yPos = HEIGHT // 2
 xDir = random.randint(0, 1)
 yDir = random.randint(0, 1)
 
-# load dvd logo
-dvd = pygame.transform.scale(pygame.image.load(DVDIMG), (DVDWIDTH, DVDHEIGHT))
+# load dvd logos
+dvdb = pygame.transform.scale(pygame.image.load("images/dvdb.png"), (DVDWIDTH, DVDHEIGHT))
+dvdc = pygame.transform.scale(pygame.image.load("images/dvdc.png"), (DVDWIDTH, DVDHEIGHT))
+dvdg = pygame.transform.scale(pygame.image.load("images/dvdg.png"), (DVDWIDTH, DVDHEIGHT))
+dvdm = pygame.transform.scale(pygame.image.load("images/dvdm.png"), (DVDWIDTH, DVDHEIGHT))
+dvdr = pygame.transform.scale(pygame.image.load("images/dvdr.png"), (DVDWIDTH, DVDHEIGHT))
+dvdw = pygame.transform.scale(pygame.image.load("images/dvdw.png"), (DVDWIDTH, DVDHEIGHT))
+dvdy = pygame.transform.scale(pygame.image.load("images/dvdy.png"), (DVDWIDTH, DVDHEIGHT))
+dvd = dvdw
 
 # setup display
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -52,12 +66,16 @@ while running:
     # bounce dvd logo
     if xPos == 0:
         xDir = 1
+        dvdColor()
     if xPos == WIDTH - DVDWIDTH:
         xDir = 0
+        dvdColor()
     if yPos == 0:
         yDir = 0
+        dvdColor()
     if yPos == HEIGHT - DVDHEIGHT:
         yDir = 1
+        dvdColor()
 
     # detect corner hit
     if (xPos == 0 or xPos == WIDTH - DVDWIDTH) and (yPos == 0 or yPos == HEIGHT - DVDHEIGHT):
