67ee39bf2c8db984826206174a40836fbdb3f324
las_r <128936184+las-r@users.noreply.github.com>
2025-07-21 15:13:51 -0400

    Add credits and comments

--- dvd-logo.js
+++ dvd-logo.js
@@ -1,9 +1,13 @@
+// dvd logo made by las-r on github
+
+// variables
 let x, y;
 let xSpeed, ySpeed;
 let dvd;
 let logoWidth = 120;
 let logoHeight = 60;
 
+// dvd logo images
 let imgs = [
   'https://raw.githubusercontent.com/las-r/dvd-logo/refs/heads/main/images/dvdb.png',
   'https://raw.githubusercontent.com/las-r/dvd-logo/refs/heads/main/images/dvdc.png',
@@ -14,15 +18,16 @@ let imgs = [
   'https://raw.githubusercontent.com/las-r/dvd-logo/refs/heads/main/images/dvdy.png'
 ];
 
+// change dvd color function
 function dvdColor() {
   let randomIndex = Math.floor(Math.random() * imgs.length);
   dvd = loadImage(imgs[randomIndex]);
 }
 
+// preload & setup
 function preload() {
   dvdColor();
 }
-
 function setup() {
   createCanvas(800, 600);
   x = random(width - logoWidth);
@@ -31,21 +36,22 @@ function setup() {
   ySpeed = 3;
 }
 
+// draw loop
 function draw() {
   background(0);
-  
+
+  // draw dvd
   if (dvd) {
     image(dvd, x, y, logoWidth, logoHeight);
   }
 
+  // dvd movement
   x += xSpeed;
   y += ySpeed;
-  
   if (x <= 0 || x + logoWidth >= width) {
     xSpeed *= -1;
     dvdColor();
   }
-  
   if (y <= 0 || y + logoHeight >= height) {
     ySpeed *= -1;
     dvdColor();
